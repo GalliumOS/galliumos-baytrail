2bc2aac3f81da232fd20537cbff3b4b8c64a5deb
Hugh Greenberg <hugegreenbug@gmail.com>
2016-04-20 23:47:59 -0600

    Improved wifi sleep script Added pulseaudio sleep script to restart audio on resume from suspend Added a baytrail service to disable async power manager for suspending

--- lib/systemd/system-sleep/wifi.sh
+++ lib/systemd/system-sleep/wifi.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-# File: "/etc/pm/sleep.d/99wifi".
 
 case $1/$2 in
     post/*)
-	/sbin/rmmod iwlmvm iwlwifi
+        /sbin/rmmod iwlmvm iwlwifi mac80211 cfg80211
 	/sbin/modprobe iwlwifi
-        systemctl restart wpa_supplicant
 	systemctl restart NetworkManager
 	;;
 esac

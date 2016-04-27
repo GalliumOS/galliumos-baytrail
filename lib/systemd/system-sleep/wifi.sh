c34597c2a13b6f18af59cd88bdfcf82fb578b2a8
Hugh Greenberg <hugegreenbug@gmail.com>
2016-04-26 21:21:07 -0600

    Changed wifi sleep script

--- lib/systemd/system-sleep/wifi.sh
+++ lib/systemd/system-sleep/wifi.sh
@@ -2,9 +2,19 @@
 
 case $1/$2 in
     post/*)
-        /sbin/rmmod iwlmvm iwlwifi mac80211 cfg80211
-	/sbin/modprobe iwlwifi
-	systemctl restart NetworkManager
+	for i in `seq 1 5`
+	do
+            rmmod iwlmvm iwlwifi mac80211 cfg80211
+            modprobe iwlwifi
+            sleep 1
+            systemctl restart NetworkManager
+	    sleep 2
+	    WIFI=`nmcli d | grep wifi`
+	    if [ -n "$WIFI" ]
+	    then
+		break
+	    fi
+	done	
 	;;
 esac
 exit 0
